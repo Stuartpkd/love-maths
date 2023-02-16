568b2876b149920ced6614b6282b75bd183e564c
Stuartpkd <stuartparkerdesign@gmail.com>
2023-02-16 16:33:33 +0000

    Set up structure of game

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCheckAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementwrongAnswer() {
+
+}
+
+function displayAdditionalQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
